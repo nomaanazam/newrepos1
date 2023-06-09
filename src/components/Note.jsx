5c28de4c218ea7f87cc0f0682d4ede49f4e9870d
nomaanazam <125955187+nomaanazam@users.noreply.github.com>
2023-06-09 14:52:06 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Monday</h1>
+      <p>My first sample note</p>
+    </div>
+  );
+}
+
+export default Note;
